511b9bdf8ddec3b9584a85e935c8984690cd27a4
onurcantasyurek <onurcantasyurek@gmail.com>
2019-02-26 22:08:31 +0300

    apply RootQuery to schema

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass a schema
+    schema
+  })
+);
 
 app.listen(4000, () => {
   console.log("Listening port: 4000");
