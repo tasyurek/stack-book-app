64fd5655cbabff2228ce284eebbab7101141f401
onurcantasyurek <onurcantasyurek@gmail.com>
2019-02-28 21:15:26 +0300

    Apply Mongoose & connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://broku:Onur60214@gql-broku-kwqjl.mongodb.net/test?retryWrites=true"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database...");
+});
 
 app.use(
   "/graphql",
