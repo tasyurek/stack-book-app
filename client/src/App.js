c005228d5a4d8294f93518a1ae8dfb9c2f37c2c3
onurcantasyurek <onurcantasyurek@gmail.com>
2019-03-01 23:13:14 +0300

    install apollo-boost & react-apollo & graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import BookList from "./components/BookList";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello clients</div>;
+    return (
+      <div className="App">
+        Book List
+        <BookList />
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
